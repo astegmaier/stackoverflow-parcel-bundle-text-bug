323261a5956127d2389255e88945d73c31e2b432
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2022-04-12 12:37:59 -0400

    Use shadow dom.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const hello = document.createElement('h1');
-hello.textContent = "Hello World!";
-document.body.append(hello);
+function appendSampleElements(element) {
+    const hello = document.createElement('h1');
+    hello.className = "hello"
+    hello.textContent = "Hello World!";
+
+    const goodbye = document.createElement('h1');
+    goodbye.className = "goodbye"
+    goodbye.textContent = "Goodbye World!";
+
+    root.append(hello);
+    root.append(goodbye);
+}
+
+const root = document.getElementById("root");
+const shadowRoot = document.getElementById("shadowroot").attachShadow({ mode: "open" })
+
+appendSampleElements(root);
+appendSampleElements(shadowRoot);
